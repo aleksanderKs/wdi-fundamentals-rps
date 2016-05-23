a4f5a8d52f619b5636a38116068cb287e18478fe
aleksanderKs <ligalizer8@gmail.com>
2016-05-22 22:03:10 -0400

    new

--- app/js/rockpaperscissors.js
+++ app/js/rockpaperscissors.js
@@ -35,7 +35,7 @@ function getComputerMove(move) {
     // Write an expression that operates on a variable called `move`
     // If a `move` has a value, your expression should evaluate to that value.
     // However, if `move` is not specified / is null, your expression should equal `randomPlay()`.
-    return move || getInput(); BLA BLA BLA
+    return move || getInput();  
 }
 
 function getWinner(playerMove,computerMove) {
